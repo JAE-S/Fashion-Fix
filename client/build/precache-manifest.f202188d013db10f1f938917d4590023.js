d1e3dffc426cc20b63a7d19c3f8a9c748a099258
Jessie S <jessie.sholler@gmail.com>
2020-02-07 17:00:32 -0500

    client folder readme.md file deleted

---
+++ client/build/precache-manifest.f202188d013db10f1f938917d4590023.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "88992a710b474aab173ad7dc5583ad2a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d888f26bded6ab615c7e",
+    "url": "/static/css/main.c1afe0ce.chunk.css"
+  },
+  {
+    "revision": "bcac9b6e876d91c096f4",
+    "url": "/static/js/2.da9b4cb9.chunk.js"
+  },
+  {
+    "revision": "40f25773c2b5a5017a2e",
+    "url": "/static/js/3.fe20bd84.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/3.fe20bd84.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0fc5b65ca853fdbb329f",
+    "url": "/static/js/4.31bc7e88.chunk.js"
+  },
+  {
+    "revision": "d888f26bded6ab615c7e",
+    "url": "/static/js/main.95db6e3e.chunk.js"
+  },
+  {
+    "revision": "06b09ce20c77e0367475",
+    "url": "/static/js/runtime-main.4592ef62.js"
+  }
+]);
